fdae88576a80be05d99e03155dafec902f13ff21
leechan <dlcks0601@gmail.com>
2024-12-24 16:26:28 +0900

    [Feat]: 테일윈드 적용 테스트

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,7 @@
 const MainPage = () => {
-  return <div>Main Page</div>;
+  return (
+    <div className='bg-blue-500 text-white text-center p-8'>Main Page</div>
+  );
 };
 
 export default MainPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    './index.html', // 프로젝트 루트의 index.html 파일
+    './src/**/*.{js,ts,jsx,tsx}', // src 폴더의 모든 JS, TS, JSX, TSX 파일
+  ],
   theme: {
     extend: {},
   },
