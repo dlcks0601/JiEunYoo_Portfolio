16efab82d8ee1ded70b8d49ddf8bbf755ec21f70
leechan <dlcks0601@gmail.com>
2025-01-19 23:05:12 +0900

    [Feat] 디자인 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,22 @@
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+
 const MainPage = () => {
   return (
-    <div className='bg-blue-500 text-white text-center p-8'>Main Page</div>
+    <div className='flex w-full justify-center min-h-screen'>
+      <div className='flex w-full flex-col p-[10px] max-w-screen-sm gap-[10px]'>
+        <div className='flex w-full justify-between items-center'>
+          <div className='flex text-[40px]'>JIEUN YOO</div>
+          <div className='flex'>
+            <MenuOutlinedIcon
+              fontSize='inherit'
+              style={{ fontSize: '48px' }}
+              className='text-black'
+            />
+          </div>
+        </div>
+        <div className='flex w-full border border-black border-[1.5px]'></div>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}', // src 폴더의 모든 JS, TS, JSX, TSX 파일
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Raleway', 'sans-serif'], // Raleway를 기본 sans 폰트로 설정
+      },
+    },
   },
   plugins: [],
 };
