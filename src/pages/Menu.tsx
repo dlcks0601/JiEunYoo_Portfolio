bea1d41fe35faddeab3bb786f0db424404c48cb5
leechan <dlcks0601@gmail.com>
2025-01-24 05:48:15 +0900

    [Feat] 메뉴 구현

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,32 @@
+const Menu = () => {
+  return (
+    <div className='flex flex-col gap-[60px]'>
+      <div className='flex gap-[20px]'>
+        <div className='text-[30px] font-semibold'>1.</div>
+        <div className='text-[30px] font-semibold'>Stick to ( )</div>
+      </div>
+      <div className='flex gap-[20px]'>
+        <div className='text-[30px] font-semibold'>2.</div>
+        <div className='text-[30px] font-semibold'>
+          Can my dance buy a house?
+        </div>
+      </div>
+      <div className='flex gap-[20px]'>
+        <div className='text-[30px] font-semibold'>3.</div>
+        <div className='text-[30px] font-semibold'>Dance to become a dance</div>
+      </div>
+      <div className='flex gap-[20px]'>
+        <div className='text-[30px] font-semibold'>4.</div>
+        <div className='text-[30px] font-semibold'>seoulforest and movers</div>
+      </div>
+      <div className='flex gap-[20px]'>
+        <div className='text-[30px] font-semibold'>5.</div>
+        <div className='text-[30px] font-semibold'>
+          ... - - - ... (morse code)
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
