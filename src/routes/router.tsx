d93569c8af3a995c07938684dcf893391b2b6161
leechan <dlcks0601@gmail.com>
2025-01-24 05:48:31 +0900

    [Refactor] 라우터 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
+import Menu from '@/pages/Menu';
+import Layout from '@/layout/Layout';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <MainPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <MainPage />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+    ],
   },
 ]);
 
